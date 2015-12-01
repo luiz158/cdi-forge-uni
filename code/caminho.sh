90e8be245a988eb3766907ee23a68f21a87c406a
Luiz Augusto <luiz158@gmail.com>
2015-12-01 16:45:41 -0200

    java 7 e java 8 ---.

---
+++ code/caminho.sh
@@ -0,0 +1,7 @@
+
+echo $JAVA_HOME 
+ls -als /usr/java
+export JAVA_HOME=/usr/java/latest-oracle-1.8
+echo $PATH 
+export PATH=$JAVA_HOME/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/home/luiz/.local/bin:/home/luiz/bin
+
